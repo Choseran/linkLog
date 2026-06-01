710aba8f430222affbcc32149a261d12ea4068ee
r-eong <ryungkyung28@gmail.com>
2026-06-01 20:09:28 +0900

    2026-06-01 메인 index 핸들러 수정, index 모달 기능구현 완료

--- src/pages/detailModal.jsx
+++ src/pages/detailModal.jsx
@@ -3,12 +3,70 @@ import { Link, useNavigate } from 'react-router-dom';
 
 import "../assets/css/style.css";
 
-export default function DetailModal({ isModalOpen, isModalClose, selectDay, selectDayContents }){
-    
+export default function DetailModal({ isModalOpen, isModalClose, selectDay, selectDayContents, getYoutubeThumbnail, saveDiaryHandle }){
+    if(!isModalOpen) return null;
 
     return(
         <div className='detailModalContainer'>
-
+            <div className='modalContainer'>
+                <button type='button' className='modalCloseBtn' onClick={isModalClose}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                    </svg>
+                    <span>닫기</span>
+                </button>
+                <div className='modalBody'>
+                    {/* 날짜표시 & 전날/다음날 이동 화살표 */}
+                    <div className='modalHeader'>
+                        <button type='button' className='prevMonthBtn'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                <path strokeLinecap="miter" strokeLinejoin="miter" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                            </svg>
+                        </button>
+                        <p className='currentMonth'>{selectDay}</p>
+                        <button type='button' className='nextMonthBtn'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                            </svg>
+                        </button>
+                    </div>
+                    {/* 유튜브 화면/미리보기 */}
+                    <div className='youtubeArea'>
+                        {selectDayContents?.youtubeUrl ? (
+                            <iframe 
+                            src={getYoutubeThumbnail(selectDayContents.youtubeUrl)} 
+                            frameBorder='0' 
+                            scrolling='no' 
+                            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                            ></iframe>
+                        )
+                        :
+                        <div className='emptyVideoArea'>
+                            <p>재생할 영상이 없습니다.</p>
+                        </div>
+                        }
+                    </div>
+                    {/* 일기 내용 */}
+                    <div className='diaryContent'>
+                        <textarea disabled>{selectDayContents?.content || '작성된 일기가 없습니다.'}</textarea>
+                    </div>
+                </div>
+            </div>
+            {/* 작성 / 수정 버튼 */}
+            <button type='button' className='writeBtn' onClick={saveDiaryHandle}>
+                {selectDayContents === null ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+                ) : (
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                    </svg>
+                )}
+            </button>
+            <div className='modalOverlay'>
+            </div>
         </div>
     )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -31,42 +31,57 @@ export default function Index(){
 
         if(!data){
             const dummy = {
-                '2026.05.10' : {
+                '2026.06.01' : {
+                    'content' : `와 진짜 레전드 사건 \n내가대체이걸어떻게해낸거냐 \n열심히검색한나자신정말고생했다!!`,
+                    'youtubeUrl' : 'https://youtu.be/wA1LKdGwFgc?si=XWKFjIp8KFGCoGJa', 
+                    'thumbnail' : 'https://img.youtube.com/vi/wA1LKdGwFgc/mqdefault.jpg',
+                },
+                '2026.06.02' : {
+                    'content' : '더미 일기0',
+                    'youtubeUrl' : 'https://youtu.be/N4FN1Fb-4i8?si=LnnJ7xTDQ6sLphZ7', 
+                    'thumbnail' : 'https://img.youtube.com/vi/N4FN1Fb-4i8/mqdefault.jpg',
+                },
+                '2026.06.10' : {
                     'content' : '더미 일기1',
                     'youtubeUrl' : 'https://youtu.be/ItSKahBISg0?si=LSYRc95a5NuGBZVx', 
                     'thumbnail' : 'https://img.youtube.com/vi/ItSKahBISg0/mqdefault.jpg',
                 },
-                '2026.05.11' : {
+                '2026.06.11' : {
                     'content' : '더미 일기2',
                     'youtubeUrl' : 'https://youtu.be/6zwJqum91vs?si=e9fCuHxP8FkQbnzQ', 
                     'thumbnail' : 'https://img.youtube.com/vi/6zwJqum91vs/mqdefault.jpg',
                 },
-                '2026.05.15' : {
+                '2026.06.13' : {
+                    'content' : '더미 일기더미 일기더미 일기더미 일기',
+                    'youtubeUrl' : 'https://youtu.be/wrHH2QjA4yM?si=IiGaIDI64_PZzt4O', 
+                    'thumbnail' : 'https://img.youtube.com/vi/wrHH2QjA4yM/mqdefault.jpg',
+                },
+                '2026.06.15' : {
                     'content' : '더미 일기3',
                     'youtubeUrl' : 'https://youtu.be/CUO2BpYPXUs?si=FNkkZam9e8vioMYc', 
                     'thumbnail' : 'https://img.youtube.com/vi/CUO2BpYPXUs/mqdefault.jpg',
                 },
-                '2026.05.19' : {
+                '2026.06.19' : {
                     'content' : '더미 일기4',
                     'youtubeUrl' : 'https://youtu.be/z1ZIWJaKWGY?si=Iv4Q2G5UJFClkdD4', 
                     'thumbnail' : 'https://img.youtube.com/vi/z1ZIWJaKWGY/mqdefault.jpg',
                 },
-                '2026.05.20' : {
-                    'content' : '더미 일기5',
+                '2026.06.20' : {
+                    'content' : '와! 정말 엄청난일이 벌어졌습니다 \n제가이걸대체어떻게만든거죠 \nai가도와주긴했습니다그래도엄청나게장하다고생각합니다 \n \n^______^',
                     'youtubeUrl' : 'https://youtu.be/EXtiYj1w7dI?si=U2h_shvXFRZdvUXT', 
                     'thumbnail' : 'https://img.youtube.com/vi/EXtiYj1w7dI/mqdefault.jpg',
                 },
-                '2026.05.21' : {
+                '2026.06.21' : {
                     'content' : '더미 일기6',
                     'youtubeUrl' : 'https://youtu.be/2lwu4ZB-d44?si=0LH_fPRtw52MEMhI', 
                     'thumbnail' : 'https://img.youtube.com/vi/2lwu4ZB-d44/mqdefault.jpg',
                 },
-                '2026.05.25' : {
+                '2026.06.25' : {
                     'content' : '더미 일기7',
                     'youtubeUrl' : 'https://youtu.be/ZoZQoYpZaso?si=PdmLzHh9IUWeJCej', 
                     'thumbnail' : 'https://img.youtube.com/vi/ZoZQoYpZaso/mqdefault.jpg',
                 },
-                '2026.05.29' : {
+                '2026.06.29' : {
                     'content' : '더미 일기8',
                     'youtubeUrl' : 'https://youtu.be/DYgE3SGPEqk?si=Ji9MVeZertqIRhGN', 
                     'thumbnail' : 'https://img.youtube.com/vi/DYgE3SGPEqk/mqdefault.jpg',
@@ -86,26 +101,6 @@ export default function Index(){
         const day = String(date.getDate()).padStart(2, '0');
 
         return `${year}.${month}.${day}`;
-    }
-
-    // 썸네일 추출, 배열에 추가 - 추후 사용
-    const getYoutubeThumbnail = (url) => {
-        if(!url) return '';
-
-        // ID 추출
-        const regID = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-        const match = url.match(regID);
-
-        if(match && match[2].length == 11){
-            const videoID = match[2];
-
-            // ID 조합해서 중화질 썸네일 반환함
-            // maxresdefault <- 이건 고화질
-            // 고화질 안 쓴 이유 : 옛날 영상이면 없을수도 있기때문
-            return `https://img.youtube.com/vi/${videoID}/mqdefault.jpg`;
-        }
-        // 올바르지 않은 링크일 경우 빈값으로 반환
-        return '';
     }
 
     // 이전 달로 이동하는 핸들러
@@ -135,11 +130,56 @@ export default function Index(){
             setSelectDayContents(diaries[dateStr]);
         }else{
             setSelectDayContents(null);
-            alert('작성된 일기가 없습니다');
+            // alert('작성된 일기가 없습니다');
         }
 
         // 모달 열기
         setIsModalOpen(true);
+    }
+
+    // 일기 추가 및 수정 핸들러
+    const saveDiaryHandle = (dateStr, url, content) => {
+        const imgUrl = '';
+        const regID = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+        const match = url.match(regID);
+        if(match && match[2].length == 11) {
+            imgUrl = `https://img.youtube.com/vi/${match[2]}/mqdefault.jpg`;
+        }
+
+        const updateDiary = {
+            ...diaries, 
+            [dateStr]: {
+                content: content, 
+                youtubeUrl: url, 
+                thumbnail: imgUrl,
+            }
+        }
+
+        // 업데이트
+        setDiaries(updateDiary);
+        localStorage.setItem('linklog_diaries', JSON.stringify(updateDiary));
+        // 갱신 -> 즉시반영
+        setSelectDayContents(updateDiary[dateStr]);
+    }
+    
+    // 썸네일 추출, 배열에 추가
+    const getYoutubeThumbnail = (url) => {
+        if(!url) return '';
+
+        // ID 추출
+        const regID = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+        const match = url.match(regID);
+
+        if(match && match[2].length == 11){
+            const videoID = match[2];
+
+            // ID 조합해서 중화질 썸네일 반환함
+            // maxresdefault <- 이건 고화질
+            // 고화질 안 쓴 이유 : 옛날 영상이면 없을수도 있기때문
+            return `https://www.youtube.com/embed/${videoID}?autoplay=0&rel=0`;
+        }
+        // 올바르지 않은 링크일 경우 빈값으로 반환
+        return '';
     }
     
     return(
@@ -202,16 +242,13 @@ export default function Index(){
                     onActiveStartDateChange={({ activeDate }) => setActiveDate(activeDate)}
                 />
             </div>
-            <button type='button' className='writeBtn'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
-                </svg>
-            </button>
             <DetailModal 
                 isModalOpen = {isModalOpen} 
                 isModalClose = {() => setIsModalOpen(false)} 
                 selectDay = {selectDay ? dataFormat(selectDay) : ''} 
-                selectDayContents = {selectDayContents}
+                selectDayContents = {selectDayContents} 
+                getYoutubeThumbnail = {getYoutubeThumbnail} 
+                saveDiaryHandle = {saveDiaryHandle}
             />
         </div>
     )
