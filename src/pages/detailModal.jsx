ee5bde582bb8fc14bf44762bdfe3c439043bf977
r-eong <ryungkyung28@gmail.com>
2026-06-03 22:07:57 +0900

    260603 정기회의 수정완료

--- src/pages/detailModal.jsx
+++ src/pages/detailModal.jsx
@@ -10,18 +10,18 @@ export default function DetailModal({ isModalOpen, isModalClose, selectDay, sele
     return(
         <div className={`detailModalContainer ${isModalOpen ? 'open' : ''}`} onClick={isModalClose}>
             <div className='modalContainer' onClick={(e) => e.stopPropagation()}>
-                <button type='button' className='modalCloseBtn' onClick={isModalClose}>
+                {/* <button type='button' className='modalCloseBtn' onClick={isModalClose}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                    </svg>
+                    </svg> */}
                     {/* <span>닫기</span> */}
-                </button>
+                {/* </button> */}
                 <div className='modalBody'>
                     {/* 날짜표시 & 전날/다음날 이동 화살표 */}
                     <div className='modalHeader'>
                         <button type='button' className='prevMonthBtn' onClick={prevDayHandler}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                                <path strokeLinecap="miter" strokeLinejoin="miter" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
                             </svg>
                         </button>
                         <p className='currentDate'>{selectDay}</p>
@@ -38,7 +38,7 @@ export default function DetailModal({ isModalOpen, isModalClose, selectDay, sele
                             src={selectDayContents.youtubeUrl.replace("watch?v=", "embed/").replace("youtu.be/", "youtube.com/embed/")} 
                             frameBorder='0' 
                             scrolling='no' 
-                            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture *"
                             allowFullScreen
                             ></iframe>
                         )
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -180,7 +180,7 @@ export default function Index(){
                     setActiveDate(prevMonth);
                 }}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                        <path strokeLinecap="miter" strokeLinejoin="miter" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
                     </svg>
                 </button>
                 <p className="currentMonth">{activeDate.getMonth() + 1}월</p>
